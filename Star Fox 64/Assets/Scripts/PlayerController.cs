c83a17720a40e90c18d956fcfafaeafd7f1348a8
Michael Surdouski <surdouski.cards@gmail.com>
2018-05-14 16:42:59 -0700

    ShipRail / Boost / Brake
    Moved ShipRail back to it's own script, added boost and brake to it
    (with smoothing from speed transitions).

--- Star Fox 64/Assets/Scripts/PlayerController.cs
+++ Star Fox 64/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool Rails_On = true;
+    private bool hardBankRightLock = false;
+    private bool hardBankLeftLock = false;
 
-    protected bool hardBankRightLock = false;
-    protected bool hardBankLeftLock = false;
-
-    protected bool boostLock = false;
-    protected bool brakeLock = false;
+    private bool boostLock = false;
+    private bool brakeLock = false;
 
     private Rigidbody rb;
 
@@ -21,17 +19,10 @@ public class PlayerController : MonoBehaviour
     private const float HARD_BANK_ANGLE = 89.0f;
     private const float HARD_BANK_ANGLE_SPEED = 5.0f;
     private const float LOOK_DIRECTION_SPEED = 1.0f;
-    private const float BOOST_SPEED = 13.0f;
+    private const float BOOST_SPEED = 30.0f;
     private const float BRAKE_SPEED = 5.0f;
 
     private float movementSpeedStep_x_y;
-    private float railSpeedStep;
-
-    private float bankAngle = 0.0f;
-    private float bankAngleSpeed;
-
-    private bool usingBoost = false;
-    private bool usingBrake = false;
 
     private bool col = false;
 
@@ -41,106 +32,112 @@ public class PlayerController : MonoBehaviour
         rb.detectCollisions = true;
         rb.freezeRotation = true;
 
-        //rail = GetComponent<Rail>();
-
         movementSpeedStep_x_y = MOVEMENT_SPEED_X_Y * Time.deltaTime;
-        railSpeedStep = RAIL_SPEED * Time.deltaTime;
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = -Input.GetAxis("Vertical");
-        //float moveForward = 0; // changes value when boosting or braking
         bool hardBankRight = Input.GetButton("HardBankRight");
         bool hardBankLeft = Input.GetButton("HardBankLeft");
         bool boost = Input.GetButton("Boost");
         bool brake = Input.GetButton("Brake");
 
-        // R||L Bumper input == false
-        if ((hardBankRight || hardBankLeft) == false)
-        {
-            hardBankRightLock = false;
-            hardBankLeftLock = false;
-            bankAngleSpeed = BANK_ANGLE_SPEED;
-
-            if (moveHorizontal > 0)
-            {
-                bankAngle = -BANK_ANGLE;
-            }
-            else if (moveHorizontal < 0)
-            {
-                bankAngle = BANK_ANGLE;
-            }
-            else
-            {
-                bankAngle = 0;
-            }
-        }
-
-        // R||L Bumper input == true
-        else
-        {
-            if ((hardBankRight == true) && (hardBankLeft == false))
-            {
-                bankAngle = -HARD_BANK_ANGLE;
-                hardBankRightLock = true;
-            }
-            else if ((hardBankLeft == true) && (hardBankRight == false))
-            {
-                bankAngle = HARD_BANK_ANGLE;
-                hardBankLeftLock = true;
-            }
-            bankAngleSpeed = HARD_BANK_ANGLE_SPEED;
-        }
+        float bankAngle = 0.0f;
+        float bankAngleSpeed = 0.0f;
 
         Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);
         Vector3 finalDirection = new Vector3(moveHorizontal, moveVertical, LOOK_DIRECTION_SPEED);
+        transform.position += direction * movementSpeedStep_x_y;
+
+        // R||L Bumper input == false; give ship banking behavior based on joystick input
+        if ((hardBankRight || hardBankLeft) == false)
+        {
+            BankAngle(moveHorizontal,moveVertical,ref bankAngle, ref bankAngleSpeed);
+        }
+
+        // R||L Bumper input == true; give ship banking behavior based on bumper input
+        else
+        {
+            HardBankAngle(hardBankRight, hardBankLeft, ref bankAngle, ref bankAngleSpeed);
+        }
 
         Quaternion look = Quaternion.LookRotation(finalDirection) * Quaternion.AngleAxis(bankAngle, Vector3.forward);
-
-        transform.position += direction * movementSpeedStep_x_y;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, look, bankAngleSpeed);
 
-        // Boost input == true
-        if (boost || brake)
+        // Boost input
+        if (boost && brake){}
+        else if (boost && !brake)
         {
-            if (boost && !brake)
-            {
-                boostLock = true;
-            }
-            else if (brake && !boost)
-            {
-                brakeLock = true;
-            }
+            boostLock = true;
+            brakeLock = false;
+        }
+        else if (brake && !boost)
+        {
+            brakeLock = true;
+            boostLock = false;
         }
         else
         {
-            boostLock = false;
             brakeLock = false;
+            boostLock = false;
         }
-
-        Rail();
     }
 
-    private void Rail()
+    /*
+     * Gives the ship a rotation based on the moveHorizontal and moveVertical inputs.
+     * */
+    private void BankAngle(float moveHorizontal, float moveVertical, ref float bank_angle, ref float bank_angle_speed)
     {
-        if (Rails_On == true)
+        hardBankRightLock = false;
+        hardBankLeftLock = false;
+        bank_angle_speed = BANK_ANGLE_SPEED;
+
+        if (moveHorizontal > 0)
         {
-            if (boostLock)
-            {
-                railSpeedStep = BOOST_SPEED * Time.deltaTime;
-            }
-            else if (brakeLock)
-            {
-                railSpeedStep = BRAKE_SPEED * Time.deltaTime;
-            }
-            else
-            {
-                railSpeedStep = RAIL_SPEED * Time.deltaTime;
-            }
-            transform.position = transform.position + transform.forward * railSpeedStep;
+            bank_angle = -BANK_ANGLE;
         }
+        else if (moveHorizontal < 0)
+        {
+            bank_angle = BANK_ANGLE;
+        }
+        else
+        {
+            bank_angle = 0;
+        }
+    }
+
+
+    /*
+     * Gives the ship a rotation based on the hardBankRight and hardBankLeft inputs.
+     * */
+    private void HardBankAngle(bool hardBankRight, bool hardBankLeft, ref float bank_angle, ref float bank_angle_speed)
+    {
+        if (hardBankRight && hardBankLeft)
+        {
+            if (hardBankRightLock)
+            {
+                bank_angle = -HARD_BANK_ANGLE;
+            }
+            else if (hardBankLeftLock)
+            {
+                bank_angle = HARD_BANK_ANGLE;
+            }
+        }
+        else if (hardBankRight)
+        {
+            bank_angle = -HARD_BANK_ANGLE;
+            hardBankRightLock = true;
+            hardBankLeftLock = false;
+        }
+        else if (hardBankLeft)
+        {
+            bank_angle = HARD_BANK_ANGLE;
+            hardBankLeftLock = true;
+            hardBankRightLock = false;
+        }
+        bank_angle_speed = HARD_BANK_ANGLE_SPEED;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -151,5 +148,30 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionExit(Collision collision)
     {
         col = false;
+    }
+
+    public bool getBoostLock()
+    {
+        return boostLock;
+    }
+
+    public bool getBrakeLock()
+    {
+        return brakeLock;
+    }
+
+    public float getBoostSpeed()
+    {
+        return BOOST_SPEED;
+    }
+
+    public float getBrakeSpeed()
+    {
+        return BRAKE_SPEED;
+    }
+
+    public float getRailSpeed()
+    {
+        return RAIL_SPEED;
     }
 }
---
+++ Star Fox 64/Assets/Scripts/ShipRail.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRail : MonoBehaviour
+{
+    public bool Rails_On = true;
+    public PlayerController pc;
+    private Vector3 velocity;
+
+    //private const float RAIL_SPEED = 10.0f;
+
+    private float railSpeedStep;
+
+    void Start ()
+    {
+        pc = GetComponent<PlayerController>();
+
+        railSpeedStep = pc.getRailSpeed() * Time.deltaTime;
+        velocity = transform.forward * railSpeedStep;
+    }
+	
+	void FixedUpdate ()
+    {
+        if (Rails_On == true)
+        {
+            if (pc.getBoostLock())
+            {
+                //railSpeedStep = pc.getBrakeSpeed() * Time.deltaTime;
+                Vector3 target = new Vector3(transform.position.x,transform.position.y,transform.position.z + pc.getBoostSpeed());
+                transform.position = Vector3.SmoothDamp(transform.position, target,ref velocity,1.0f);
+            }
+            else if (pc.getBrakeLock())
+            {
+                //railSpeedStep = pc.getBrakeSpeed() * Time.deltaTime;
+                Vector3 target = new Vector3(transform.position.x, transform.position.y, transform.position.z + pc.getBrakeSpeed());
+                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 1.0f);
+            }
+            else
+            {
+                //railSpeedStep = pc.getRailSpeed() * Time.deltaTime;
+                Vector3 target = new Vector3(transform.position.x,transform.position.y,transform.position.z + pc.getRailSpeed());
+                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 1.0f);
+            }
+            //transform.position = transform.position + transform.forward * railSpeedStep;
+        }
+    }
+}
