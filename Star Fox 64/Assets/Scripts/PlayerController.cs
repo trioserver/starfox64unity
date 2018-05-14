320984cdda9785a25b3528a72c6fca25918bfed3
Michael Surdouski <surdouski.cards@gmail.com>
2018-05-13 22:00:10 -0700

    Boost + Brake
    Need to re-edit the code to include separate rail script again. Going to
    do a makeover on the code tomorrow to get it set up for better
    development.

--- Star Fox 64/Assets/Scripts/PlayerController.cs
+++ Star Fox 64/Assets/Scripts/PlayerController.cs
@@ -2,87 +2,147 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public bool Rails_On = true;
 
-    public float speed;
+    protected bool hardBankRightLock = false;
+    protected bool hardBankLeftLock = false;
+
+    protected bool boostLock = false;
+    protected bool brakeLock = false;
 
     private Rigidbody rb;
-    private bool col = false;
-    private float bankAngle = 0.0f;
-    private float hardBankAngle = 89.0f;
-    private float bankAngleSpeed;
-    private bool hardBankRightLock = false;
-    private bool hardBankLeftLock = false;
 
-	// Use this for initialization
-	void Start () {
+    private const float MOVEMENT_SPEED_X_Y = 10.0f;
+    private const float RAIL_SPEED = 10.0f;
+    private const float BANK_ANGLE = 50.0f;
+    private const float BANK_ANGLE_SPEED = 2.5f;
+    private const float HARD_BANK_ANGLE = 89.0f;
+    private const float HARD_BANK_ANGLE_SPEED = 5.0f;
+    private const float LOOK_DIRECTION_SPEED = 1.0f;
+    private const float BOOST_SPEED = 13.0f;
+    private const float BRAKE_SPEED = 5.0f;
+
+    private float movementSpeedStep_x_y;
+    private float railSpeedStep;
+
+    private float bankAngle = 0.0f;
+    private float bankAngleSpeed;
+
+    private bool usingBoost = false;
+    private bool usingBrake = false;
+
+    private bool col = false;
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
         rb.detectCollisions = true;
         rb.freezeRotation = true;
-	}
 
-    // Update is called once per frame
-    void FixedUpdate() {
+        //rail = GetComponent<Rail>();
+
+        movementSpeedStep_x_y = MOVEMENT_SPEED_X_Y * Time.deltaTime;
+        railSpeedStep = RAIL_SPEED * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = -Input.GetAxis("Vertical");
+        //float moveForward = 0; // changes value when boosting or braking
         bool hardBankRight = Input.GetButton("HardBankRight");
         bool hardBankLeft = Input.GetButton("HardBankLeft");
+        bool boost = Input.GetButton("Boost");
+        bool brake = Input.GetButton("Brake");
 
-        if ((hardBankRight || hardBankLeft) != true)
+        // R||L Bumper input == false
+        if ((hardBankRight || hardBankLeft) == false)
         {
             hardBankRightLock = false;
             hardBankLeftLock = false;
-            bankAngleSpeed = 2.5f;
+            bankAngleSpeed = BANK_ANGLE_SPEED;
 
             if (moveHorizontal > 0)
             {
-                bankAngle = -50.0f;
+                bankAngle = -BANK_ANGLE;
             }
             else if (moveHorizontal < 0)
             {
-                bankAngle = 50.0f;
+                bankAngle = BANK_ANGLE;
             }
             else
             {
-                bankAngle = 0.0f;
+                bankAngle = 0;
             }
         }
+
+        // R||L Bumper input == true
         else
         {
             if ((hardBankRight == true) && (hardBankLeft == false))
             {
-                bankAngle = hardBankAngle;
+                bankAngle = -HARD_BANK_ANGLE;
                 hardBankRightLock = true;
             }
             else if ((hardBankLeft == true) && (hardBankRight == false))
             {
-                bankAngle = -hardBankAngle;
+                bankAngle = HARD_BANK_ANGLE;
                 hardBankLeftLock = true;
             }
-            /**
-            else
-            {
-                if (hardBankRightLock == true)
-                {
-                    bankAngle = hardBankAngle;
-                }
-                else
-                {
-                    bankAngle = -hardBankAngle;
-                }
-            }
-            */
-            bankAngleSpeed = 5.0f;
+            bankAngleSpeed = HARD_BANK_ANGLE_SPEED;
         }
-        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);
-        Vector3 finalDirection = new Vector3(moveHorizontal, moveVertical, 1.0f);
-        Quaternion look = Quaternion.LookRotation(finalDirection);
-        look = look*Quaternion.AngleAxis(bankAngle, Vector3.forward);
 
-        transform.position += direction * speed * Time.deltaTime;
+        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);
+        Vector3 finalDirection = new Vector3(moveHorizontal, moveVertical, LOOK_DIRECTION_SPEED);
+
+        Quaternion look = Quaternion.LookRotation(finalDirection) * Quaternion.AngleAxis(bankAngle, Vector3.forward);
+
+        transform.position += direction * movementSpeedStep_x_y;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, look, bankAngleSpeed);
 
+        // Boost input == true
+        if (boost || brake)
+        {
+            if (boost && !brake)
+            {
+                boostLock = true;
+            }
+            else if (brake && !boost)
+            {
+                brakeLock = true;
+            }
+        }
+        else
+        {
+            boostLock = false;
+            brakeLock = false;
+        }
+
+        Rail();
     }
+
+    private void Rail()
+    {
+        if (Rails_On == true)
+        {
+            if (boostLock)
+            {
+                railSpeedStep = BOOST_SPEED * Time.deltaTime;
+            }
+            else if (brakeLock)
+            {
+                railSpeedStep = BRAKE_SPEED * Time.deltaTime;
+            }
+            else
+            {
+                railSpeedStep = RAIL_SPEED * Time.deltaTime;
+            }
+            transform.position = transform.position + transform.forward * railSpeedStep;
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         col = true;
