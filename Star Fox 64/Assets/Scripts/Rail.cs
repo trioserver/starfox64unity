80907060257e920606d66d9128ab1d07e4093c67
Michael Surdouski <surdouski.cards@gmail.com>
2018-05-13 17:33:14 -0700

    Rail movement, including camera following player ship
    .

---
+++ Star Fox 64/Assets/Scripts/Rail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public bool Rails_On = true;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Rails_On == true)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = transform.position + transform.forward*step;
+        }
+	}
+}
